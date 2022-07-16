b230979eb52d48860f371633e65e2471a758ae0d
JPNAKAYAMA <jpn_rufino@hotmail.com>
2022-07-16 09:59:33 -0300

    myFinances - first commit

---
+++ my-finances/components/FinancesTable.tsx
@@ -0,0 +1,119 @@
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Box,
+  Button,
+  Heading,
+  IconButton,
+} from "@chakra-ui/react";
+import { api } from "../services/api";
+import { IExpenses } from "../models/IExpenses";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+
+interface IOnAddExpense {
+  expenses: IExpenses[]
+  onAddExpense: () => void;
+  onEditExpense: (expense: IExpenses) => void;
+  onRemoveExpense: (expense: IExpenses) => void;
+}
+
+const FinancesTable = ({ expenses, onAddExpense, onEditExpense, onRemoveExpense }: IOnAddExpense) => {
+
+  const toBRL = (value: number) =>
+    value.toLocaleString("pt-BR", {
+      currency: "BRL",
+      style: "currency",
+    });
+
+  const total = expenses.reduce((acc, cur) => {
+    return acc + cur.value
+  }, 0)
+
+  return (
+    <>
+      <Box
+        flexDirection="row"
+        display="flex"
+        w="80vw"
+        justifyContent="space-between"
+      >
+        <Heading size="lg">
+          {/* Total: {toBRL(expenses?.reduce((acc, curr) => acc + curr.value, 0))} */}
+          Total: {toBRL(total)}
+        </Heading>
+        <Button
+          bg="green.400"
+          color="white"
+          _hover={{ bg: "green.300" }}
+          _active={{ bg: "green.600" }}
+          onClick={() => onAddExpense()}
+        >
+          Adicionar despesa
+        </Button>
+      </Box>
+      <Box
+        mt={5}
+        border="1px solid"
+        borderColor="gray.100"
+        borderWidth={2}
+        borderRadius="md"
+        w="80vw"
+      >
+        <TableContainer>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th isNumeric>ID</Th>
+                <Th>DATA</Th>
+                <Th>DESCRIÇÃO</Th>
+                <Th>CATEGORIA</Th>
+                <Th>VALOR</Th>
+                <Th>AÇÕES</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {expenses.map((expense) => (
+                <Tr key={expense.id}>
+                  <Td isNumeric>{expense.id}</Td>
+                  <Td>{new Date(expense.date).toLocaleDateString("pt-BR")}</Td>
+                  <Td>{expense.description}</Td>
+                  <Td>{expense.category}</Td>
+                  <Td>{toBRL(expense.value)}</Td>
+                  <Td>
+                    <IconButton 
+                      aria-label="Editar"
+                      icon={<EditIcon />}
+                      color="yellow.500"
+                      variant="ghost"
+                      onClick={() => {
+                        onEditExpense(expense)
+                      }}
+                    />
+                    <IconButton 
+                      aria-label="Remover"
+                      icon={<DeleteIcon />}
+                      color="red.600"
+                      variant="ghost"
+                      onClick={() =>{
+                        onRemoveExpense(expense)
+                      }}
+                    />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </>
+  );
+};
+
+export default FinancesTable;
---
+++ my-finances/pages/index.tsx
@@ -0,0 +1,77 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import FinancesTable from "../components/FinancesTable";
+import { Heading, useDisclosure } from "@chakra-ui/react";
+import NewExpenseModal from "../components/NewExpenseModal";
+import { useEffect, useState } from "react";
+import { IExpenses } from "../models/IExpenses";
+import { api, getExpenses, removeExpense } from "../services/api";
+
+const Home: NextPage = () => {
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const [expenses, setExpenses] = useState<IExpenses[]>([]);
+  const [expenseToEdit, setExpenseToEdit] = useState<IExpenses>()
+
+  const fetchExpenses = () =>
+  getExpenses().then(expenseList => setExpenses(expenseList))
+
+  useEffect(() => {
+    fetchExpenses
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>My finances</title>
+        <meta name="description" content="Vnt 4tech 2022" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <Heading as="h2" mb="100px">
+          My Finances
+        </Heading>
+        <FinancesTable
+          expenses={expenses}
+          onAddExpense={() => onOpen()}
+          onEditExpense={(expense) => {
+            setExpenseToEdit(expense)
+            onOpen();
+          }}
+          onRemoveExpense={async (expense) =>{
+            await removeExpense(expense)
+            fetchExpenses()
+          }}
+        />
+        <NewExpenseModal
+          isOpen={isOpen}
+          expense={expenseToEdit}
+          onSave={() => {
+            fetchExpenses()
+            onClose()
+            setExpenseToEdit(undefined)
+          }}
+          onClose={() =>{
+            onClose()
+            setExpenseToEdit(undefined)
+          }}
+        />
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://www.venturus.org.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by Venturus
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
